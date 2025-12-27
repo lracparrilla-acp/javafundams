37d946b851bb3b54879f0c25aafb692d541ba7e1
lracparrilla-acp <lr.acparrilla@mmdc.mcl.edu.ph>
2025-12-27 19:04:32 +0800

    dec 27

---
+++ src/exercises/tempconverterTernary.java
@@ -0,0 +1,18 @@
+package exercises;
+
+import java.util.Scanner;
+
+/* temperature converter using the ternary operator. Prompts the user to enter the temperature (double) and the unit (C or F). Let's x  */
+
+
+public class tempconverterTernary {
+    static void main() {
+
+        int value, newValue;
+        String unit;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("");
+    }
+
+
+}
